48f641c086b698517d0301a66b99d3aa2c4901ec
kavyakunder <kavyakunder7082@gmail.com>
2024-12-08 21:31:35 +0530

    refactor dropdown modal

---
+++ src/app/components/QuestionTypeDropdown.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { HiMiniBars2 } from "react-icons/hi2";
+import { HiMiniBars3BottomLeft } from "react-icons/hi2";
+import { MdOutlineRadioButtonChecked } from "react-icons/md";
+import { PiLinkSimple } from "react-icons/pi";
+import { LuCalendarDays } from "react-icons/lu";
+
+interface QuestionTypeDropdownProps {
+    handleUpdateQuestionType: (type: string) => void;
+}
+
+const questionTypes = [
+    { type: 'short-answer', label: 'Short Answer', icon: <HiMiniBars2 size={20} /> },
+    { type: 'long-answer', label: 'Long Answer', icon: <HiMiniBars3BottomLeft size={20} /> },
+    { type: 'single-select', label: 'Single Select', icon: <MdOutlineRadioButtonChecked size={20} /> },
+    { type: 'url', label: 'URL', icon: <PiLinkSimple size={20} /> },
+    { type: 'date', label: 'Date', icon: <LuCalendarDays size={20} /> },
+];
+
+
+const QuestionTypeDropdown = ({ handleUpdateQuestionType }: QuestionTypeDropdownProps) => {
+    return (
+        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+            <div className="bg-white w-[300px] p-6 rounded-xl shadow-lg h-[224px] border-2 border-gray-medium">
+                <h2 className="text-xs font-semibold mb-4 bg-[#fafbfc] text-[#6A737D]">INPUT TYPES</h2>
+                <div
+                    className='flex flex-col gap-2 text-sm'>
+                    {questionTypes.map(({ type, label, icon }) =>
+                        <div
+                            onClick={() => handleUpdateQuestionType(type)}
+                            className='flex items-center gap-2 hover:cursor-pointer'>
+                            {icon}
+                            <span>{label}</span>
+                        </div>
+                    )}
+
+                </div >
+            </div>
+        </div>
+    )
+}
+
+export default QuestionTypeDropdown
